063e4f9f0abdfa46822b97685c1995dff19e3e3d
Tim Carlson <timgcarlson@gmail.com>
2022-05-16 21:30:26 -0700

    ready for PR test deployment to vercel

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,10 @@ export default function Home() {
   return (
     <div>
       <h1>The Home Page</h1>
-      <Link href="/pluto-moons">Pluto Moons</Link>
+      <h2 className={styles.subtitle}>This is an open pull request!</h2>
+      <Link href="/pluto-moons">
+        <a>Pluto Moons</a>
+      </Link>
     </div>
   )
 }
